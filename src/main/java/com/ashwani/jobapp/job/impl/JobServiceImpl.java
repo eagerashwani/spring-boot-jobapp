10c5ee768ef2ce6594c568a48a30b10f358ad4af
eagerashwani <eagerashwani@gmail.com>
2023-12-07 19:59:06 +0530

    Job Service Impl

--- src/main/java/com/ashwani/jobapp/job/impl/JobServiceImpl.java
+++ src/main/java/com/ashwani/jobapp/job/impl/JobServiceImpl.java
@@ -1,8 +1,60 @@
 package com.ashwani.jobapp.job.impl;
 
+import com.ashwani.jobapp.job.Job;
+import com.ashwani.jobapp.job.JobRepository;
 import com.ashwani.jobapp.job.JobService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class JobServiceImpl implements JobService {
+
+    JobRepository jobRepository;
+
+    public JobServiceImpl(JobRepository jobRepository) {
+        this.jobRepository = jobRepository;
+    }
+
+    @Override
+    public List<Job> allJobs() {
+        return jobRepository.findAll();
+    }
+
+    @Override
+    public Job oneJob(Long id) {
+        return jobRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void createJob(Job job) {
+        jobRepository.save(job);
+    }
+
+    @Override
+    public boolean removeJobById(Long id) {
+        Optional<Job> optionalJob = jobRepository.findById(id);
+        if(optionalJob.isPresent()){
+            jobRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean updateJob(Long id, Job updatedJob) {
+        Optional<Job> jobOptional = jobRepository.findById(id);
+        if(jobOptional.isPresent()){
+            Job job = jobOptional.get();
+            job.setTitle(updatedJob.getTitle());
+            job.setDescription(updatedJob.getDescription());
+            job.setLocation(updatedJob.getLocation());
+            job.setMaxSalary(updatedJob.getMaxSalary());
+            job.setMinSalary(updatedJob.getMinSalary());
+            jobRepository.save(job);
+            return true;
+        }
+        return false;
+    }
 }
