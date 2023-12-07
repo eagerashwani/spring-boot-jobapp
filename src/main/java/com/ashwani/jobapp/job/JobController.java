af1a6b267043b17127c11844291921da4476b5e2
eagerashwani <eagerashwani@gmail.com>
2023-12-07 19:47:28 +0530

    Job Entity class

---
+++ src/main/java/com/ashwani/jobapp/job/JobController.java
@@ -0,0 +1,9 @@
+package com.ashwani.jobapp.job;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/jobs")
+public class JobController {
+}
---
+++ src/main/java/com/ashwani/jobapp/job/JobService.java
@@ -0,0 +1,7 @@
+package com.ashwani.jobapp.job;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface JobService {
+}
