5a74c545997d85fe99b89ea38c085d8f67823f50
eagerashwani <eagerashwani@gmail.com>
2023-12-07 21:11:34 +0530

    Job Controller

--- src/main/java/com/ashwani/jobapp/job/JobController.java
+++ src/main/java/com/ashwani/jobapp/job/JobController.java
@@ -1,9 +1,55 @@
 package com.ashwani.jobapp.job;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/jobs")
 public class JobController {
+
+    JobService jobService;
+
+    public JobController(JobService jobService) {
+        this.jobService = jobService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Job>> findAll(){
+        List<Job> jobs = jobService.allJobs();
+        return new ResponseEntity<>(jobs, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Job> findOne(@PathVariable Long id){
+        Job job = jobService.oneJob(id);
+        if(job != null)
+            return new ResponseEntity<>(job, HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createJob(@PathVariable Job job){
+        jobService.createJob(job);
+        return new ResponseEntity<>("Job added successfully", HttpStatus.OK);
+    }
+
+    @PutMapping
+    public ResponseEntity<String> updateJob(@PathVariable Long id, @PathVariable Job job){
+        boolean updated = jobService.updateJob(id, job);
+        if(updated)
+            return new ResponseEntity<>("Job updated Successfully", HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> removeJob(@PathVariable Long id){
+        boolean removed = jobService.removeJobById(id);
+        if(removed)
+            return new ResponseEntity<>("Job removed Successfully", HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+    }
 }
