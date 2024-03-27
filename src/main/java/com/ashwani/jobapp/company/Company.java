be26964365792472bcc6b527bd039d10c5811228
eagerashwani <eagerashwani@gmail.com>
2024-03-27 19:52:19 +0530

    all changes

--- src/main/java/com/ashwani/jobapp/company/Company.java
+++ src/main/java/com/ashwani/jobapp/company/Company.java
@@ -1,6 +1,7 @@
 package com.ashwani.jobapp.company;
 
 import com.ashwani.jobapp.job.Job;
+import com.ashwani.jobapp.review.Review;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
@@ -15,9 +16,20 @@ public class Company {
     private String name;
     private String location;
 
-    @JsonIgnore
-    @OneToMany(mappedBy = "company")
+    @JsonIgnore // ignore krna hai iss field ko, Infinite recursion error aa rhi thi
+    @OneToMany(mappedBy = "company", cascade = CascadeType.ALL) // ye map ho rha hai, Job class k company se, Iss Table mey ye field nhi bnega
     private List<Job> jobs;
+
+    @OneToMany(mappedBy = "company") // one company has many reviews
+    private List<Review> reviews;
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
 
     public Company() {
     }
